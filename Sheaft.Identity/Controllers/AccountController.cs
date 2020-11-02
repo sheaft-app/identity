782071c3e0882f1798f2e12db214fb846b084fc2
Noël Mugnier <noel.mugnier@outlook.com>
2020-11-02 20:02:59 +0100

    Fix - Correction du register avec une connexion externe

--- Sheaft.Identity/Controllers/AccountController.cs
+++ Sheaft.Identity/Controllers/AccountController.cs
@@ -376,16 +376,21 @@ namespace Sheaft.Identity.Controllers
         public async Task<IActionResult> Register(string returnUrl, string username = null)
         {
             // build a model so we know what to show on the login page
-            var vm = await BuildRegisterViewModelAsync(returnUrl);
-            vm.Username = vm.Username ?? username;
-
+            var vm = await BuildLoginViewModelAsync(returnUrl);
             if (vm.IsExternalLoginOnly)
             {
                 // we only have one option for logging in and it's an external provider
                 return RedirectToAction("Challenge", "External", new { provider = vm.ExternalLoginScheme, returnUrl });
             }
 
-            return View(vm);
+            return View(new RegisterViewModel
+            {
+                AllowRememberLogin = vm.AllowRememberLogin,
+                EnableLocalLogin = vm.EnableLocalLogin,
+                ReturnUrl = vm.ReturnUrl,
+                Username = vm.Username ?? username,
+                ExternalProviders = vm.ExternalProviders
+            });
         }
 
         [HttpPost]
@@ -634,69 +639,16 @@ namespace Sheaft.Identity.Controllers
 
         private async Task<RegisterViewModel> BuildRegisterViewModelAsync(RegisterInputModel model)
         {
-            var vm = await BuildRegisterViewModelAsync(model.ReturnUrl);
-            vm.Username = model.Username;
-            vm.FirstName = model.FirstName;
-            vm.LastName = model.LastName;
-            vm.RememberLogin = model.RememberLogin;
-            return vm;
-        }
-
-        private async Task<RegisterViewModel> BuildRegisterViewModelAsync(string returnUrl)
-        {
-            var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
-            if (context?.IdP != null && await _schemeProvider.GetSchemeAsync(context.IdP) != null)
-            {
-                var local = context.IdP == IdentityServer4.IdentityServerConstants.LocalIdentityProvider;
-
-                // this is meant to short circuit the UI and only trigger the one external IdP
-                var vm = new RegisterViewModel
-                {
-                    EnableLocalLogin = local,
-                    ReturnUrl = returnUrl,
-                    Username = context?.LoginHint,
-                };
-
-                if (!local)
-                {
-                    vm.ExternalProviders = new[] { new ExternalProvider { AuthenticationScheme = context.IdP } }.ToList();
-                }
-
-                return vm;
-            }
-
-            var schemes = await _schemeProvider.GetAllSchemesAsync();
-
-            var providers = schemes
-                .Where(x => x.DisplayName != null)
-                .Select(x => new ExternalProvider
-                {
-                    DisplayName = x.DisplayName ?? x.Name,
-                    AuthenticationScheme = x.Name
-                }).ToList();
-
-            var allowLocal = true;
-            if (context?.Client.ClientId != null)
-            {
-                var client = await _clientStore.FindEnabledClientByIdAsync(context.Client.ClientId);
-                if (client != null)
-                {
-                    allowLocal = client.EnableLocalLogin;
-
-                    if (client.IdentityProviderRestrictions != null && client.IdentityProviderRestrictions.Any())
-                    {
-                        providers = providers.Where(provider => client.IdentityProviderRestrictions.Contains(provider.AuthenticationScheme)).ToList();
-                    }
-                }
-            }
-
+            var vm = await BuildLoginViewModelAsync(model.ReturnUrl);
             return new RegisterViewModel
             {
-                AllowRememberLogin = AccountOptions.AllowRememberLogin,
-                EnableLocalLogin = allowLocal && AccountOptions.AllowLocalLogin,
-                ReturnUrl = returnUrl,
-                Username = context?.LoginHint,
-                ExternalProviders = providers.ToArray()
+                AllowRememberLogin = vm.AllowRememberLogin,
+                EnableLocalLogin = vm.EnableLocalLogin,
+                ReturnUrl = vm.ReturnUrl,
+                Username = vm.Username,
+                ExternalProviders = vm.ExternalProviders,
+                FirstName = model.FirstName,
+                LastName = model.LastName
             };
         }
 
